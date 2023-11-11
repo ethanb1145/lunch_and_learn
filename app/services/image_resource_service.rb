344938c90528015fe59ebd707e9bfa4d80f91d9d
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-11-11 13:48:03 -0700

    Complete learning resources endpoint

--- app/services/image_resource_service.rb
+++ app/services/image_resource_service.rb
@@ -12,6 +12,21 @@ class ImageResourceService
 
   def images(country)
     results = get_url("v1/search?query=#{country}")
-    results.first(10)
+    urls = extract_urls(results)
+    alt_tags = extract_alt_tags(results)
+    
+    images_data = alt_tags.first(10).zip(urls.first(10)).map do |alt_tag, url|
+      { alt_tag: alt_tag, url: url }
+    end
+
+    images_data
+  end
+
+  def extract_urls(results)
+    results[:photos].map { |photo| photo[:url] }
+  end
+
+  def extract_alt_tags(results)
+    results[:photos].map { |photo| photo[:alt] }
   end
 end
