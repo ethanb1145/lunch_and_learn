efaa1e91cb133db835d2adf0a7ace6a22c4b0dc4
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-11-11 15:28:31 -0700

    Webmock adjustments

--- spec/requests/api/v1/learning_resources_by_country_spec.rb
+++ spec/requests/api/v1/learning_resources_by_country_spec.rb
@@ -3,6 +3,9 @@ require "rails_helper"
 RSpec.describe "Learning Resources API", type: :request do
   describe "GET /api/v1/learning_resources" do
     it "returns learning resources for a specific country" do
+      WebMock.allow_net_connect!
+      json_data = File.read("spec/fixtures/estonia.json")
+      stub_request(:get, "http.localhost:3000/api/v1/learning_resources?country=estonia").to_return(status: 200, body: json_data, headers: {})
       get "/api/v1/learning_resources", params: { country: "Estonia" }
 
       expect(response).to have_http_status(200)
