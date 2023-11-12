77bf9538ac9afece987c31cb86f37ab590fca8a3
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-11-12 15:32:40 -0700

    Complete US6

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,4 +1,16 @@
 class Api::V1::FavoritesController < ApplicationController
+
+  def index
+    user = User.find_by(api_key: params[:api_key])
+
+    if user
+      favorites = user.favorites
+      render json: { data: favorites_response(favorites) }
+    else
+      render json: { error: ["Invalid Api-Key."] }, status: 400
+    end
+  end
+
   def create
     user = User.find_by(api_key: params[:api_key])
 
@@ -19,5 +31,20 @@ class Api::V1::FavoritesController < ApplicationController
 
   def favorite_params
     params.permit(:country, :recipe_link, :recipe_title)
+  end
+
+  def favorites_response(favorites)
+    favorites.map do |favorite|
+      {
+        id: favorite.id.to_s,
+        type: "favorite",
+        attributes: {
+          recipe_title: favorite.recipe_title,
+          recipe_link: favorite.recipe_link,
+          country: favorite.country,
+          created_at: favorite.created_at.strftime("%Y-%m-%dT%H:%M:%SZ")
+        }
+      }
+    end
   end
 end
