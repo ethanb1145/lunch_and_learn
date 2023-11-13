3a058eac70a7bf1b8cc0d6df27e24e887bd99938
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-11-13 13:43:08 -0700

    Make test change

--- spec/requests/api/v1/learning_resources_by_country_spec.rb
+++ spec/requests/api/v1/learning_resources_by_country_spec.rb
@@ -1,47 +1,33 @@
 require "rails_helper"
 
-RSpec.describe "Learning Resources API", type: :request do
-  describe "GET /api/v1/learning_resources" do
-    it "returns learning resources for a specific country" do
+RSpec.describe "Recipes API", type: :request do
+  describe "GET /api/v1/recipes" do
+    it "returns recipes for a specific country" do
       WebMock.allow_net_connect!
-      
-      json_data = File.read("spec/fixtures/estonia.json")
-      stub_request(:get, "http://localhost:3000/api/v1/learning_resources?country=estonia").to_return(status: 200, body: json_data, headers: {})
-      
-      get "/api/v1/learning_resources", params: { country: "Estonia" }
+
+      get "/api/v1/recipes", params: { country: "Thailand" }
 
       expect(response).to have_http_status(200)
       json_response = JSON.parse(response.body)
-      
-      expect(json_response["data"]).to be_an(Hash)
-      
-      expected_response = {
-        "data" => {
-          "attributes" => {
-            "country" => "Estonia",
-            "images" => [
-              {"alt_tag" => "White and Brown Concrete Building", "url" => "https://www.pexels.com/photo/white-and-brown-concrete-building-1803860/"},
-              {"alt_tag" => "Photo of Castle Exterior", "url" => "https://www.pexels.com/photo/photo-of-castle-exterior-3224113/"},
-              {"alt_tag" => "Body of Water", "url" => "https://www.pexels.com/photo/body-of-water-2177955/"},
-              {"alt_tag" => "White and Brown Concrete Building", "url" => "https://www.pexels.com/photo/white-and-brown-concrete-building-1803859/"},
-              {"alt_tag" => "White and Brown Concrete Building", "url" => "https://www.pexels.com/photo/white-and-brown-concrete-building-1803858/"},
-              {"alt_tag" => "Brown Deer on Green Grass Field", "url" => "https://www.pexels.com/photo/brown-deer-on-green-grass-field-3732496/"},
-              {"alt_tag" => "Low Angle Photo of High-rise Building", "url" => "https://www.pexels.com/photo/low-angle-photo-of-high-rise-building-1140989/"},
-              {"alt_tag" => "", "url" => "https://www.pexels.com/photo/lighthouse-on-tahkuna-peninsula-19011015/"},
-              {"alt_tag" => "Close Up Photo of Text on Brown Paper", "url" => "https://www.pexels.com/photo/close-up-photo-of-text-on-brown-paper-6957221/"},
-              {"alt_tag" => "Photo of Person Holding Dj Controller", "url" => "https://www.pexels.com/photo/photo-of-person-holding-dj-controller-1493004/"}
-            ],
-            "video" => {
-              "title" => "A Super Quick History of Estonia",
-              "youtube_video_id" => "pKhdxYGw83I"
-            }
-          },
-          "id" => nil,
-          "type" => "learning_resource"
-        }
-      }
+      expect(json_response["data"]).to be_an(Array)
+      recipe = json_response["data"].first
+      expect(recipe["attributes"]).to include("title", "url", "country", "image")
+    end
 
-      expect(json_response).to eq(expected_response)
+    it "returns recipes for a random country if none is provided" do
+      get "/api/v1/recipes"
+
+      expect(response).to have_http_status(200)
+      json_response = JSON.parse(response.body)
+      expect(json_response["data"]).to be_an(Array)
+    end
+
+    it "returns an empty array if the country parameter is empty or a value that does not return any recipes" do
+      get "/api/v1/recipes", params: { country: "#+==_-=-" }
+
+      expect(response).to have_http_status(200)
+      json_response = JSON.parse(response.body)
+      expect(json_response["data"]).to eq([])
     end
   end
 end
